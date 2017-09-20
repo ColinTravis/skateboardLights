4f9d256ddf57f4861ca3e7f0d22e8e6e24c1faf5
Colin Travis <colint@cybertravis.us>
2017-09-19 22:32:15 -0500

    Changing branches

--- skateboard.ino
+++ skateboard.ino
@@ -6,13 +6,32 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM303_U.h>
+#include <Adafruit_NeoPixel.h>
+#include <SPI.h>
+
+
+#define BRAKETHRESHOLD        350
+#define BRAKETIMETHRESHOLD    200
+
+
+int start = 0;
+
+int prevX = 0;
+int currentX = 0;
+
+int cState = 0;
+int dState = 0;
+
+long brakeTime = 0;
 
 /* Assign a unique ID to this sensor at the same time */
-Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
+
+Adafruit_LSM303 lsm;
+
+// Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
 
 ////////////
 // NEOPIXEL
-#include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
@@ -30,144 +49,84 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(totalLEDs, PIN, NEO_GRB + NEO_KHZ800
 
 
 void setup(void) {
+  Serial.begin(9600);
   strip.begin();
   strip.setBrightness(100);
   strip.show(); // Initialize all pixels to 'off'
 
-// ===========================
-// ----- ACCELEROMETER -------
-// ===========================
+// =================================
+// ----- SETUP ACCELEROMETER -------
+// =================================
 
-
-#ifndef ESP8266
-  while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
-#endif
-  Serial.begin(9600);
-  Serial.println("Accelerometer Test"); Serial.println("");
-
-  /* Initialise the sensor */
-  if(!accel.begin())
+ /* Initialise the sensor */
+  if(!lsm.begin())
   {
     /* There was a problem detecting the ADXL345 ... check your connections */
     Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
     while(1);
   }
 
-  /* Display some basic information on this sensor */
-  displaySensorDetails();
+} //END SETUP
 
-}
+
+/////////////
+// MAIN LOOP
+
 
 void loop(void) {
-  rgbFadeInAndOut(255, 50, 0, ledFadeTime); // Orange
-  rgbFadeInAndOut(255, 50, 0, ledFadeTime); // Orange
-   allLights(strip.Color(255, 0, 0, 0), 3000); // Red
-   theaterChase(strip.Color(32, 107, 255), 75); // Blue
 
-//   colorWipe(strip.Color(255, 83, 13), 50); // Orange
+  lsm.read();
+  currentX = abs(lsm.accelData.x);
 
-//   theaterChase(strip.Color(255, 255, 255), 70); // White
-
-
-// ===========================
-// ----- ACCELEROMETER -------
-// ===========================
-
-  /* Get a new sensor event */
-  sensors_event_t event;
-  accel.getEvent(&event);
-
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
-  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
-  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
-
-  /* Note: You can also get the raw (non unified values) for */
-  /* the last data sample as follows. The .getEvent call populates */
-  /* the raw values used below. */
-  //Serial.print("X Raw: "); Serial.print(accel.raw.x); Serial.print("  ");
-  //Serial.print("Y Raw: "); Serial.print(accel.raw.y); Serial.print("  ");
-  //Serial.print("Z Raw: "); Serial.print(accel.raw.z); Serial.println("");
-
-  /* Delay before the next sample */
-  delay(500);
-
-
-
-}
-
-
-
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
+  if (start == 0){
+    prevX = currentX;
+    start = 1;
   }
-}
 
-void allLights(uint32_t c, uint16_t wait) {
-  for(int i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-  }
-  delay(wait);
-}
+  int i = currentX - prevX;
 
-
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
+  if (abs(i) > BRAKETHRESHOLD) {
+    brakeTime = millis();
+    int strikes = 0;
+    while ((abs(i) > BRAKETHRESHOLD) && (strikes < 3)) {
+      if (abs(i) < BRAKETHRESHOLD) {
+        strikes = strikes + 1;
       }
-      strip.show();
+      lsm.read();
+      currentX = abs(lsm.accelData.x);
+      i = currentX - prevX;
 
-      delay(wait);
-
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      if ((millis() - brakeTime) > BRAKETIMETHRESHOLD) {
+        brakeLights(strip.Color(255, 0, 0, 0), 250); // Red
+        while (abs(i) > BRAKETHRESHOLD) {
+          lsm.read();
+          currentX = abs(lsm.accelData.x);
+          i = currentX - prevX;
+          Serial.println(i);
+          delay(100);
+        }
+        // hideAll();
+        idleLights(255, 50, 0, ledFadeTime); // Orange
+        brakeTime = millis();
+        i = 0;
+        lsm.read();
+        currentX = abs(lsm.accelData.x);
       }
     }
   }
+
+  prevX = currentX;
+  delay(200);
+
 }
 
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
 
-//////////////////////////
-//////////////////////////
 
-void rgbFadeInAndOut(uint8_t red, uint8_t green, uint8_t blue, uint8_t wait) {
-  for(uint8_t b = 0; b <255; b++) {
-     for(uint8_t i=0; i < strip.numPixels(); i++) {
-        strip.setPixelColor(i, red * b/255, green * b/255, blue * b/255);
-     }
 
-     strip.show();
-     delay(wait);
-  };
+  // idleLights(255, 50, 0, ledFadeTime); // Orange
+  // idleLights(255, 50, 0, ledFadeTime); // Orange
+  // brakeLights(strip.Color(255, 0, 0, 0), 3000); // Red
+  // motionLights(strip.Color(32, 107, 255), 75); // Blue
 
-  for(uint8_t b=255; b > 0; b--) {
-     for(uint8_t i = 0; i < strip.numPixels(); i++) {
-        strip.setPixelColor(i, red * b/255, green * b/255, blue * b/255);
-     }
-     strip.show();
-     delay(wait);
-  };
-};
+  // colorWipe(strip.Color(255, 83, 13), 50); // Orange
+  // theaterChase(strip.Color(255, 255, 255), 70); // White
